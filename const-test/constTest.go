19a57c4c1005275b0cef11ffddf1eeea57c014d8
luo2pei4 <luo2pei4321@outlook.com>
2020-09-12 15:34:53 +0900

    commit

---
+++ const-test/constTest.go
@@ -0,0 +1,20 @@
+// 本程序演示常量中的数据类型只可以是布尔，数字（整数、浮点和复数）和字符串类型
+package main
+
+import "fmt"
+
+// B is a bool type const var
+const B = true
+
+// Pi is an numeric type const var
+const Pi = 3.145926
+
+// Str is a string type const var
+const Str = "Hello world"
+
+func main() {
+
+	fmt.Println(B)
+	fmt.Println(Pi)
+	fmt.Println(Str)
+}
