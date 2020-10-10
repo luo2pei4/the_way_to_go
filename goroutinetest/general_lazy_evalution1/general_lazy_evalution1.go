1b50521ec900111d0443b9818851643955e44ec2
luo2pei4 <luo2pei4321@outlook.com>
2020-10-10 16:15:08 +0900

    commit

--- goroutinetest/general_lazy_evalution1/general_lazy_evalution1.go
+++ goroutinetest/general_lazy_evalution1/general_lazy_evalution1.go
@@ -33,7 +33,6 @@ func main() {
 // 两个匿名函数loopFunc和retFunc，loopFunc函数在协程中调用，retFunc函数则用于返回
 // loopFunc函数被创建为协程。在一个无限循环中获取传入函数的计算结果，并将其中一个计算结果放入retValChan通道内
 // retFunc是一个从retValChan通道获取数据的函数。如果该函数不被调用，retValChan通道将被阻塞。
-//
 func BuildLazyEvaluator(evalFunc EvalFunc, initState Any) func() Any {
 
 	retValChan := make(chan Any)
